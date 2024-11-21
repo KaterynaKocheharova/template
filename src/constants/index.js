e5021cfd8e983c7e3c8c823bc24d569be274df87
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-21 09:28:35 +0300

    prepare template

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,25 +9,6 @@ export const UPLOAD_DIR = path.join(process.cwd(), "uploads");
 // -templates
 export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
 
-
-// ======================================= PAGINATION / FILTRATION /  SORTING
-// - available sorting orders
-export const SORT_ORDER = {
-  ASC: 'asc',
-  DESC: 'desc',
-};
-// - filtration is available for:
-export const keysOfContact = [
-  'name',
-  'phoneNumber',
-  'email',
-  '_id',
-  'createdAt',
-  'updatedAt',
-];
-export const contactTypes = ['work', 'home', 'personal'];
-
-
 // ============================= NUMBERS
 // - access token
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,8 @@
 import express from "express";
-import contactsRouter from "./contacts.js";
-import authRouter from "./auth.js";
 
 const router = express.Router();
 
-router.use("/contacts", contactsRouter);
-router.use("/auth", authRouter);
+// router.use("/contacts", );
+// router.use("/auth", );
 
 export default router;
